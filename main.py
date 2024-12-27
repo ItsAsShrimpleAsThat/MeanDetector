74257b2bdb8f185bd170b6a76788b3523bafd025
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2024-12-26 17:51:53 -0800

    add discord bot

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from openai import OpenAI
+from discord.ext import commands
+from discord import app_commands
+import discord
 
-keyFile = open("api.key", "r") # open API key file.
-apiKey = keyFile.read()
+gptKeyFile = open("gpt.key", "r") # open API key file.
+gptAPIKey = gptKeyFile.read()
+gptKeyFile.close()
 
 client = OpenAI(
-  api_key=apiKey
+  api_key = gptAPIKey
 )
 
+discordKeyFile = open("discord.key", "r")
+discordAPIkey = discordKeyFile.read()
+discordKeyFile.close()
+
+client = commands.Bot(command_prefix = ")", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("The Mean Detector is ready to eliminate meanness")
+    try:
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except:
+        print("ur codes fucking broken lmao")
+
+@client.tree.command(name="enable")
+async def enable(interaction: discord.Interaction):
+    await interaction.response.send_message("please fucking work i beg you")
+
+client.run(discordAPIkey)
+
+"""
 completion = client.chat.completions.create(
   model="gpt-4o-mini",
   store=True,
@@ -16,3 +42,4 @@ completion = client.chat.completions.create(
 )
 
 print(completion.choices[0].message.content);
+"""
